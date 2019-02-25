88f85bd4aa0235f6ac76ac07132cda3fbb57e1a6
Vermote Tiemen <tiemen.vermote@student.howest.be>
2019-02-25 22:06:47 +0100

    Add project files.

---
+++ full-MVC-form-validation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace full_MVC_form_validation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
